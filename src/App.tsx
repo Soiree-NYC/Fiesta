f138eec525e16117b733a9a195c337cafee8f734
Fezz <jonnicwolf@gmail.com>
2024-10-04 18:01:20 -0400

    Imports Home to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './styles.css';
 
 import NavBar from './components/ui/layout/NavBar';
+import Home from './components/pages/Home';
 
 function App() {
   return (
     <>
       <NavBar />
+      <Home />
     </>
   );
 };
