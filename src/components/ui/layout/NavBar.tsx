646f64a39f595e98c219800dc87224bb86fe1bda
Fezz <jonnicwolf@gmail.com>
2024-10-04 12:27:52 -0400

    Adds spacing and layout

--- src/components/ui/layout/NavBar.tsx
+++ src/components/ui/layout/NavBar.tsx
@@ -1,19 +1,19 @@
 const NavBar = () => {
   return (
-    <nav className='bg-gray-200'>
-      <div>
-        logo
+    <nav className='flex justify-between m-4'>
+      <div className='px-8 text-3xl'>
+        🎉
       </div>
       
-      <div>
-        <button></button>
-        <button></button>
+      <div className='flex gap-4 px-8'>
+        <button>Partify your business</button>
+        <button>🌐</button>
         <button>
           sign up group
         </button>
       </div>
     </nav>
   );
-};
+};``
 
 export default NavBar;
