c78797a7c6bf9cd5909f1480d884241f6e9ac1fe
Fezz <jonnicwolf@gmail.com>
2024-10-09 19:09:16 -0400

    "Adds media query for med screen grid cols from 5 - 3"

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -3,11 +3,9 @@ import Feed from "../ui/cards/Feed";
 import SimpleCard from "../ui/cards/SimpleCard";
 
 const Home = () => {
-  const items  = ['blep','blep','blep','blep','blep','blep','blep','blep',
-  // <button className='hover: border-b-4'>blep</button>,<button className='hover: border-b-4'>blep</button>,<button className='hover: border-b-4'>blep</button>,<button className='hover: border-b-4'>blep</button>,<button className='hover: border-b-4'>blep</button>,<button className='hover: border-b-4'>blep</button>,<button className='hover: border-b-4'>blep</button>,<button className='hover: border-b-4'>blep</button>,<button className='hover:border-b-4'>blep</button>,<button className='hover: border-b-4'>blep</button>,
-];
+  const items  = ['blep','blep','blep','blep','blep','blep','blep','blep',];
 
-  const feedItems = [<SimpleCard />,<SimpleCard />,<SimpleCard />,<SimpleCard />,<SimpleCard />,<SimpleCard />,<SimpleCard />,<SimpleCard />,<SimpleCard />,<SimpleCard />,<SimpleCard />,<SimpleCard />,<SimpleCard />,<SimpleCard />,<SimpleCard />,]
+  const feedItems = [<SimpleCard />,<SimpleCard />,<SimpleCard />,<SimpleCard />,<SimpleCard />,<SimpleCard />,<SimpleCard />,<SimpleCard />,<SimpleCard />,]
 
   return (
     <main className="px-8">
