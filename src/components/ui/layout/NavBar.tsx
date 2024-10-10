ca2951cfcc11577ef53aede714549fc0ee897887
Fezz <jonnicwolf@gmail.com>
2024-10-10 13:05:44 -0400

    Changes AccountAccess menu from white to transparent blur

--- src/components/ui/layout/NavBar.tsx
+++ src/components/ui/layout/NavBar.tsx
@@ -23,16 +23,16 @@ const NavBar = () => {
             Book Now</button>
           <AccountAccess setter={setAccountAccessToggle} getter={accountAccessToggle} />
           {accountAccessToggle && (
-            <div className="absolute right-10 mt-10 w-48 bg-white border border-gray-200 rounded-md shadow-lg">
+            <div className="absolute right-12 top-6 mt-10 w-48 bg-transparent backdrop-blur-md border border-gray-200 rounded-md shadow-lg z-20">
               <ul className="py-2 text-xs">
-                <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">Sign Up</li>
-                <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">Log In</li>
+                <li className="px-4 py-2 hover:underline hover:underline-offset-4 decoration-slate-50 cursor-pointer">Sign Up</li>
+                <li className="px-4 py-2 hover:underline hover:underline-offset-4 decoration-slate-50 cursor-pointer">Log In</li>
               </ul>
               <hr />
               <ul className="py-2 text-xs">
-                <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">Gift Cards</li>
-                <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">Partify your business</li>
-                <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">Help Center</li>
+                <li className="px-4 py-2 hover:underline hover:underline-offset-4 decoration-slate-50 cursor-pointer">Gift Cards</li>
+                <li className="px-4 py-2 hover:underline hover:underline-offset-4 decoration-slate-50 cursor-pointer">Partify your business</li>
+                <li className="px-4 py-2 hover:underline hover:underline-offset-4 decoration-slate-50 cursor-pointer">Help Center</li>
               </ul>
             </div>
           )}
