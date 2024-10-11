1ad870e619943b4f909571838363dc7ceceb6dad
Fezz <jonnicwolf@gmail.com>
2024-10-11 12:37:30 -0400

    "Updates Home component to use array of SimpleCard components with populated props instead of empty SimpleCard components."

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -5,8 +5,79 @@ import SimpleCard from "../ui/cards/SimpleCard";
 const Home = () => {
   const items  = ['blep','blep','blep','blep','blep','blep','blep','blep',];
 
-  const feedItems = [<SimpleCard />,
-    <SimpleCard />,<SimpleCard />,<SimpleCard />,<SimpleCard />,<SimpleCard />,<SimpleCard />,<SimpleCard />,<SimpleCard />,
+  const feedItems = [
+    <SimpleCard
+      title='Hotel Beacon'
+      description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      neighborhood='UWS'
+      price={80}
+      price_was={120}
+      image='https://images.pexels.com/photos/1058277/pexels-photo-1058277.jpeg'
+    />,
+    <SimpleCard
+      title='Hotel Beacon'
+      description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      neighborhood='UWS'
+      price={80}
+      price_was={120}
+      image='https://images.pexels.com/photos/1058277/pexels-photo-1058277.jpeg'
+    />,
+    <SimpleCard
+      title='Hotel Beacon'
+      description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      neighborhood='UWS'
+      price={80}
+      price_was={120}
+      image='https://images.pexels.com/photos/1058277/pexels-photo-1058277.jpeg'
+    />,
+    <SimpleCard
+      title='Hotel Beacon'
+      description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      neighborhood='UWS'
+      price={80}
+      price_was={120}
+      image='https://images.pexels.com/photos/1058277/pexels-photo-1058277.jpeg'
+    />,
+    <SimpleCard
+      title='Hotel Beacon'
+      description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      neighborhood='UWS'
+      price={80}
+      price_was={120}
+      image='https://images.pexels.com/photos/1058277/pexels-photo-1058277.jpeg'
+    />,
+    <SimpleCard
+      title='Hotel Beacon'
+      description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      neighborhood='UWS'
+      price={80}
+      price_was={120}
+      image='https://images.pexels.com/photos/1058277/pexels-photo-1058277.jpeg'
+    />,
+    <SimpleCard
+      title='Hotel Beacon'
+      description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      neighborhood='UWS'
+      price={80}
+      price_was={120}
+      image='https://images.pexels.com/photos/1058277/pexels-photo-1058277.jpeg'
+    />,
+    <SimpleCard
+      title='Hotel Beacon'
+      description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      neighborhood='UWS'
+      price={80}
+      price_was={120}
+      image='https://images.pexels.com/photos/1058277/pexels-photo-1058277.jpeg'
+    />,
+    <SimpleCard
+      title='Hotel Beacon'
+      description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      neighborhood='UWS'
+      price={80}
+      price_was={120}
+      image='https://images.pexels.com/photos/1058277/pexels-photo-1058277.jpeg'
+    />,
   ];
 
   return (
