53cca9462c419da8c85d43dea5275f580fae9a98
Fezz <jonnicwolf@gmail.com>
2024-10-20 12:36:41 -0400

    Mocks feed data, adds types for Venue etc

--- src/shared/types/venue.ts
+++ src/shared/types/venue.ts
@@ -1,14 +1,18 @@
 import { Features } from '../../shared/enums/featureDetails.ts';
+import NYCNeighborhoods from './NYC_Neighborhoods.ts';
 
-type venue = {
-  id: number;
-  name: string;
+type Venue = {
   address: string;
-  rating: number;
+  basePrice: number;
+  description: string;
   features: Features;
   host: string;
   hightlights: string[],
+  id: number;
+  name: string;
+  neighborhood: NYCNeighborhoods;
   photos: string[],
+  rating: number;
 };
 
-export type { venue };
+export type { Venue };
