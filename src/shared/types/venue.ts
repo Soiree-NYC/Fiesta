851d5b93aacded7f3f7cef23bbe21822a9df77b8
Fezz <jonnicwolf@gmail.com>
2024-10-21 14:51:29 -0400

    "Refactors type Venue, updates venues.json to reflect."

--- src/shared/types/venue.ts
+++ src/shared/types/venue.ts
@@ -4,24 +4,23 @@ import { Time } from './Time.ts';
 import { WriteUps } from './WriteUps.ts';
 
 type Venue = {
-  address: string;
-  basePrice: number;
-  description: string;
-  email: string;
-  features: FeatureKeys[];
-  host: string;
-  hallmarks: string[],
   id: number;
   name: string;
-  neighborhood: NYCNeighborhoods;
-  photos: string[],
-  rating: number;
-  reviews: number;
-  standing: number;
-  sitting: number;
-  available: boolean;
+  address: string;
+  description: string;
   open_at: Time;
   close_at: Time;
+  email: string;
+  host: string;
+  rating: number;
+  available: boolean;
+  basePrice: number;
+  standing: number;
+  sitting: number;
+  hallmarks: string[],
+  photos: string[],
+  features: FeatureKeys[];
+  neighborhood: NYCNeighborhoods;
   writeUps: WriteUps;
 };
 
