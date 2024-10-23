4ed03ef776df43e7835828536e22ca209bcb2296
Fezz <jonnicwolf@gmail.com>
2024-10-22 21:51:43 -0400

    "Adds new route for Test page and CheckoutConfig adds to layout by implementing tabs"

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import NavBar from './components/ui/layout/NavBar';
 
 import Home from './components/pages/Home';
 import VenueDetails from './components/pages/VenueDetails';
+import Test from './components/pages/Test';
 
 function App() {
   const [listItems, setListItems] = useState<number>(0);
@@ -17,6 +18,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home venues={{getter: listItems, setter: setListItems}}/>} />
           <Route path='/venue/:id' element={<VenueDetails />} />
+          <Route path='/test' element={<Test />} />
         </Routes>
       </div>
     </div>
--- src/components/ui/cards/CheckoutConfig.tsx
+++ src/components/ui/cards/CheckoutConfig.tsx
@@ -15,55 +15,64 @@ const CheckoutConfig: FC<Props> = ({ basePrice, standing }) => {
   const [guests, setGuests] = useState<number>(10);
 
   return (
-    <div className='flex flex-col border rounded-2xl bg-slate-100 p-6 gap-2 h-[30rem]'>
-      <div className='self-start flex'>
-        <h1 className='text-3xl font-bold'>${basePrice}</h1>
-        <p className='self-end'>/guest</p>
+    <>
+      <div className="flex">
+        <div className='bg-red-500 h-12 rounded-t-2xl w-1/3 z-0'/>
+        <div className='bg-green-300 h-12 rounded-t-2xl w-1/3 z-0'/>
+        <div className='bg-yellow-100 h-12 rounded-t-2xl w-1/3 z-0'/>
+        
       </div>
 
-      <div className='border rounded-2xl flex flex-col'>
-        <div className='grid grid-cols-2'>
-          <div className='flex flex-col border-r-2 p-2'>
-            <span className="text-sm self-start px-1">SOIR&#201;E</span>
-            <Time />
-          </div>
-          <div className="flex flex-col p-2">
-            <span className="text-sm self-start px-1">AFTERPARTY</span>
-            <Time />
-          </div>
+      <div className='flex flex-col border rounded-b-2xl bg-slate-100 p-6 gap-2 h-[30rem]'>
+        <div className='self-start flex'>
+          <h1 className='text-3xl font-bold'>${basePrice}</h1>
+          <p className='self-end'>/guest</p>
         </div>
-        <div className="border-t-2 p-2 flex justify-between items-center">
-          <span className="text-md">GUESTS</span>
-          <Counter count={guests} setCount={setGuests} min={10} max={standing} />
-        </div>
-      </div>
 
-      <div className='flex flex-col'>
-        <Special title='Reserve' callback={()=> console.log('yo')} />
-        <span className='self-center py-4'>You won't be charged yet</span>
+        <div className='border rounded-2xl flex flex-col'>
+          <div className='grid grid-cols-2'>
+            <div className='flex flex-col border-r-2 p-2'>
+              <span className="text-sm self-start px-1">SOIR&#201;E</span>
+              <Time />
+            </div>
+            <div className="flex flex-col p-2">
+              <span className="text-sm self-start px-1">AFTERPARTY</span>
+              <Time />
+            </div>
+          </div>
+          <div className="border-t-2 p-2 flex justify-between items-center">
+            <span className="text-md">GUESTS</span>
+            <Counter count={guests} setCount={setGuests} min={10} max={standing} />
+          </div>
+        </div>
+
         <div className='flex flex-col'>
-          <div className='flex justify-between'>
-            <span className='px-1'>${basePrice} x {guests} guests</span>
-            <span className='px-1'>${guests * basePrice}</span>
-          </div>
-          <div className='flex justify-between'>
-            <span className='px-1'>Gratuity</span>
-            <span className='px-1'>${((guests * basePrice)/100)*20}</span>
-          </div>
-          <div className='flex justify-between'>
-            <span className='px-1 line-through'>Service fee</span>
-            <span className='px-1'>$0 </span>
+          <Special title='Reserve' callback={()=> console.log('yo')} />
+          <span className='self-center py-4'>You won't be charged yet</span>
+          <div className='flex flex-col'>
+            <div className='flex justify-between'>
+              <span className='px-1'>${basePrice} x {guests} guests</span>
+              <span className='px-1'>${guests * basePrice}</span>
+            </div>
+            <div className='flex justify-between'>
+              <span className='px-1'>Gratuity</span>
+              <span className='px-1'>${((guests * basePrice)/100)*20}</span>
+            </div>
+            <div className='flex justify-between'>
+              <span className='px-1 line-through'>Service fee</span>
+              <span className='px-1'>$0 </span>
+            </div>
           </div>
         </div>
-      </div>
 
-      <hr />
+        <hr />
 
-      <div className='flex justify-between'>
-        <strong>Total before taxes</strong>
-        <strong>${guests * 80 + ((guests * basePrice)/100)*20}</strong>
+        <div className='flex justify-between'>
+          <strong>Total before taxes</strong>
+          <strong>${guests * 80 + ((guests * basePrice)/100)*20}</strong>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
