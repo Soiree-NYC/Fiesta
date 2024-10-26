b1a0685573201d68d4b6a7cfc0bb1e747cb94244
Jon Narine <jon.wolf.narine@gmail.com>
2024-10-26 01:11:23 -0400

    "Adds EarlyAccess to Test page with content"

--- src/components/pages/Test.tsx
+++ src/components/pages/Test.tsx
@@ -1,6 +1,9 @@
+import EarlyAccess from "../ui/modals/EarlyAccess";
+
 const Test = () => {
   return (
     <div className="h-[100vh] ">
+      <EarlyAccess />
     </div>
   );
 };
--- src/components/ui/modals/EarlyAccess.tsx
+++ src/components/ui/modals/EarlyAccess.tsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const EarlyAccess = () => {
   return (
-    <div>EarlyAccess</div>
+    <div className='text-white font-roboto w-full leading-none flex flex-col gap-10'>
+      <h1 className='text-[7rem] font-bold'>
+        Soirée helps nyc bars and restaurants fill their private event spaces.
+      </h1>
+
+      <p className='text-5xl'>
+        Soirée is the only website built to connect NYC bars and restaurants with customers look to book priate event. We manage themarketing and scheduling process - all you have to do is wait for bookings to come through the door.
+      </p>
+
+    </div>
   )
 }
 
