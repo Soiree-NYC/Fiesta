9663cbe4f22e9d4c6c79dba76d558083fb9a9e31
Jon Narine <jon.wolf.narine@gmail.com>
2024-10-26 02:09:56 -0400

    "Updates App.tsx to conditionally render NavBar, finishes EarlyAccess with content."

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import './styles.css';
 
 import NavBar from './components/ui/layout/NavBar';
@@ -10,11 +10,20 @@ import Test from './components/pages/Test';
 
 function App() {
   const [listItems, setListItems] = useState<number>(0);
+  const { pathname } = useLocation();
+  const exclustion_list = [
+    '/test',
+  ];
 
+  const checkExclustions = (list:string[]) => {
+    return !list.includes(pathname);
+  };
+
+  console.log(checkExclustions(exclustion_list))
   return (
     <div className='bg-skyline bg-cover bg-center flex justify-center overflow-auto'>
       <div className='max-w-screen-xl flex flex-col backdrop-blur-sm h-full pt-10'>
-        <NavBar listItems={listItems} />
+        {checkExclustions(exclustion_list) && <NavBar listItems={listItems} />}
         <Routes>
           <Route path='/' element={<Home venues={{getter: listItems, setter: setListItems}}/>} />
           <Route path='/venue/:id' element={<VenueDetails />} />
--- src/components/ui/modals/EarlyAccess.tsx
+++ src/components/ui/modals/EarlyAccess.tsx
@@ -1,18 +1,39 @@
-import React from 'react'
+
 
 const EarlyAccess = () => {
   return (
-    <div className='text-white font-roboto w-full leading-none flex flex-col gap-10'>
-      <h1 className='text-[7rem] font-bold'>
-        Soirée helps nyc bars and restaurants fill their private event spaces.
+    <div className='text-xl text-white font-roboto w-full leading-none flex flex-col gap-20 p-28 bg-black bg-opacity-50'>
+      <h1 className='text-[6rem] font-bold'>
+        Soirée helps NYC bars and restaurants fill their private event spaces.
       </h1>
 
-      <p className='text-5xl'>
+      <p>
         Soirée is the only website built to connect NYC bars and restaurants with customers look to book priate event. We manage themarketing and scheduling process - all you have to do is wait for bookings to come through the door.
       </p>
 
+      <div className='flex flex-col gap-5'>
+        <h1 className='font-bold'>EARLY ACCESS</h1>
+
+        <p>
+          We are offering bars and restaurants Early Acccess to our platform now, ahdead of our public launch in the new year. By signing up ahead of Dec 30, you can lock in the below benefits:
+
+          <ul className='list-disc list-inside pt-10'>
+            <li className='px-10'>No cost to list until the public launch, and then 6 month free</li>
+            <li className='px-10'>Soirée select status, which gives your venue a prominent position on our site and makes it show up first in customers' seatch results</li>
+          </ul>
+        </p>
+      </div>
+      <div className='flex flex-col gap-5'>
+        <h1 className='font-bold'>SOIRÉE COSTS</h1>
+          <p>
+            If enrolled in the Early Access program, you don't pay until launch, and then you'll have the option to continue listing with 6 months free! To list your bar or restaurant on Soirée, we charge $100/month, or $50/month with an annual subscription. When a customer books a private event, we'll charge 5% of the minimum spend or total open bar cost (e.g., if you have a $2,000 deposit, we will charge you a $100 booking fee).
+          </p>
+      </div>
+      <img
+        className='h-10'
+        src="/olive_group_gray.svg" alt="" />
     </div>
-  )
-}
+  );
+};
 
 export default EarlyAccess;
