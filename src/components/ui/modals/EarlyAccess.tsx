31d35c03dc752eaff4408d92b91961e49824b427
Jon Narine <jon.wolf.narine@gmail.com>
2024-10-26 04:11:09 -0400

    "Updates Primary button component to accept additional props and modifies EarlyAccess to use new Primary button"

--- src/components/ui/modals/EarlyAccess.tsx
+++ src/components/ui/modals/EarlyAccess.tsx
@@ -1,15 +1,30 @@
+import { useNavigate } from "react-router-dom";
+import Primary from "../buttons/Primary";
 
 
 const EarlyAccess = () => {
+  const navigate = useNavigate();
   return (
-    <div className='text-xl text-white font-roboto w-dvm leading-none flex flex-col gap-10 bg-black bg-opacity-50 px-20 py-10'>
-      <h1 className='text-[5rem] font-bold'>
+    <div className='text-4xl text-white font-thin font-roboto w-dvm leading-none flex flex-col gap-32 bg-black bg-opacity-50 px-10 py-10'>
+      <h1 className='text-[7rem] font-bold'>
         Soirée helps NYC bars and restaurants fill their private event spaces.
       </h1>
 
       <p>
         Soirée is the only website built to connect NYC bars and restaurants with customers look to book priate event. We manage themarketing and scheduling process - all you have to do is wait for bookings to come through the door.
       </p>
+
+      <div className='flex flex-col gap-5'>
+        <h1 className='font-bold'>HOW IT WORKS</h1>
+
+        <ul className='list-decimal list-inside leading-10'>
+          <li>You list your private event space on our site.</li>
+          <li>We market your space to customers looking to book a private event.</li>
+          <li>You communicate with customers on our platform via email or text to finalize the booking.</li>
+          <li>We collect payments for deposits/minimums on your behalf.</li>
+          <li>You host the event!</li>
+        </ul>
+      </div>
 
       <div className='flex flex-col gap-5'>
         <h1 className='font-bold'>EARLY ACCESS</h1>
@@ -29,9 +44,14 @@ const EarlyAccess = () => {
             If enrolled in the Early Access program, you don't pay until launch, and then you'll have the option to continue listing with 6 months free! To list your bar or restaurant on Soirée, we charge $100/month, or $50/month with an annual subscription. When a customer books a private event, we'll charge 5% of the minimum spend or total open bar cost (e.g., if you have a $2,000 deposit, we will charge you a $100 booking fee).
           </p>
       </div>
-      <img
-        className='h-10'
-        src="/olive_group_gray.svg" alt="" />
+
+      <div className="text-4xl">
+        <strong>
+          Join the Spaniard, the Wren, Judy Z's, and dozens of other NYC bars and restaurants signed up for Soirée today; Early Access is only available until December 30.
+        </strong>
+      </div>
+
+      <Primary link="/home" img="/olive_group_gray.svg" frosted bold/>
     </div>
   );
 };
